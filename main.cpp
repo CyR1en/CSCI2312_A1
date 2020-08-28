cde7556633c1d888e86a950bf434f3e9c27f39f9
CyR1en <cyrien3519@gmail.com>
2020-08-27 20:11:17 -0600

    Documentation

--- main.cpp
+++ main.cpp
@@ -4,21 +4,48 @@
 
 using namespace std;
 
+/**
+ * Class the represents a point on a Cartesian Plane.
+ */
 class Point {
 private:
     double x_;
     double y_;
 public:
-    Point(double x, double y) : x_(x), y_(y) {}
+    /**
+     * 2 param constructor for this class.
+     *
+     * @param x x-coordinate value for the point.
+     * @param y y-coordinate value for the point.
+     */
+    Point(double x, double y) {
+        x_ = x;
+        y_ = y;
+    }
 
+    /**
+     * Accessor for the x-coordinate of the point.
+     *
+     * @return x-coordinate of the point.
+     */
     double getX() const {
         return x_;
     }
 
+    /**
+     * Accessor for the y-coordinate of the point.
+     *
+     * @return y-coordinate of the point.
+     */
     double getY() const {
         return y_;
     }
 
+    /**
+     * String representation of the point for easy printing.
+     *
+     * @return string representation of the point.
+     */
     string toString() const {
         std::stringstream ss;
         ss << "(" << getX() << ", " << getY() << ")";
@@ -26,50 +53,80 @@ public:
     }
 };
 
+/**
+ * Class that represents a Rectangle.
+ *
+ * This implementation relies heavily on the top-left point of the graph and the length and width
+ * of the rectangle--to initialize all of the other points.
+ */
 class Rectangle {
 private:
     Point top_left_;
     Point bot_left_;
     Point bot_right_;
     Point top_right_;
-
 public:
-
+    /**
+     * Rectangle Constructor.
+     *
+     * This constructor initializes the class using initializer list because the class Point does
+     * not have a default constructor.
+     *
+     * @param topLeft Top-left point of the rectangle.
+     * @param length length of the rectangle.
+     * @param width width of the rectangle.
+     */
     Rectangle(const Point &topLeft, int length, int width) :
             top_left_(topLeft),
             top_right_(Point(top_left_.getX() + width, top_left_.getY())),
             bot_left_(Point(top_left_.getX(), top_left_.getY() - length)),
             bot_right_(Point(top_left_.getX() + width, top_left_.getY() - length)) {}
 
+    /**
+     * const reference accessor for the top-left point of the rectangle for immutability.
+     * (Have a different function handle mutability of points.)
+     *
+     * @return top-left point of the rectangle.
+     */
     const Point &getTopLeft() const {
         return top_left_;
     }
 
+    /**
+     * const reference accessor for the bottom-right point of the rectangle for immutability.
+     * (Have a different function handle mutability of points.)
+     *
+     * @return bottom-right point of the rectangle.
+     */
     const Point &getBotRight() const {
         return bot_right_;
     }
 
+    /**
+     * Function that checks if this rectangle intersects with another rectangle.
+     *
+     * Condition 1: If the left edge of this rect is to the right of rect2's right edge, then this
+     *              rectangle is to the right of rect2.
+     * Condition 2: Condition 1 but left and right are switched so this rectangle is to the left
+     *              of rect2.
+     * Condition 3: If the top edge of this rect is below rect2's bottom edge, then this rectangle
+     *              is below rect2
+     * Condition 4: Condition 3 but top and bottom switched so this rect is on top.
+     *
+     * If all conditions are not met then rects are intersecting.
+     *
+     * De Morgan's law: Not (Cond1 or Cond2 or Cond3 or Cond 4) is Not Cond1 and Not Cond2 and Not
+     *                  Not Cond3 and Not Cond4
+     *
+     * @param rect2 Another rectangle to check for collision.
+     * @return true if this rectangle intersects param rect2, false if not.
+     */
     bool intersects(Rectangle &rect2) {
-        /*
-        Condition 1: If the left edge of this rect is to the right of rect2's right edge, then this
-                     rectangle is to the right of rect2.
-        Condition 2: Condition 1 but left and right are switched so this rectangle is to the left
-                     of rect2.
-        Condition 3: If the top edge of this rect is below rect2's bottom edge, then this rectangle
-                     is below rect2
-        Condition 4: Condition 3 but top and bottom switched so this rect is on top.
-
-        If all conditions are not met then rects are intersecting.
-        
-        De Morgan's law: Not (Cond1 or Cond2 or Cond3 or Cond 4) is Not Cond1 and Not Cond2 and Not
-                         Not Cond3 and Not Cond4
-        */
         double r2LEdge = rect2.getTopLeft().getX(); // rect2 left edge.
         double r2TEdge = rect2.getTopLeft().getY(); // rect2 top edge.
 
         double r2REdge = rect2.getBotRight().getX(); // rect2 right edge.
         double r2BEdge = rect2.getBotRight().getY(); // rect2 bottom edge.
-
 
         return (top_left_.getX() < r2REdge && // !(cond1)
                 bot_right_.getX() > r2LEdge && // !(cond2)
@@ -77,6 +134,14 @@ public:
                 bot_right_.getY() > r2TEdge); // !(cond4)
     }
 
+    /**
+     * String representation of this rectangle for easier printing.
+     *
+     * This returns all the corner points of the rectangle and ordered according to the
+     * instructions on Canvas--(top-left), (bot-left), (bot-right), (top-right).
+     *
+     * @return string representation of this rectangle.
+     */
     string toString() {
         stringstream ss;
         ss << top_left_.toString();
@@ -87,8 +152,24 @@ public:
     }
 };
 
+
+/**
+ * Class that drives the implemented classes above.
+ *
+ * Made a different class for the actual assignment (Because I think it looks cleaner).
+ *
+ * All functions are static to make it easier to access from outside the class and there are
+ * no class state that I have to keep track of anyways.
+ */
 class Assignment1 {
 public:
+
+    /**
+     * Actual procedure for the assignment is in this function.
+     *
+     * This is a single shot execution of assignment 1. The instruction from Canvas was
+     * carefully followed and used the same value to get the same output.
+     */
     static void start() {
         cout << "Please input data for the first rectangle" << endl;
         Rectangle r1 = askForRectInput();
@@ -100,15 +181,34 @@ public:
         cout << "\nRectangle 2 Points:" << endl;
         cout << r2.toString() << "\n" << endl;
 
-        string intersectOut = collided(r1, r2) ? "The two rectangles intersect."
-                                               : "The two rectangles does not intersect.";
+        string intersectOut = rectangles_intersect(r1, r2) ? "The two rectangles intersect."
+                                                           : "The two rectangles does not intersect.";
         cout << intersectOut << endl;
     }
 
-    static bool collided(Rectangle &r1, Rectangle &r2) {
+    /**
+     * Checks if two rectangles intersect.
+     *
+     * This doesn't have any implementation and just uses the intersects function inside of
+     * the Rectangle class.
+     *
+     * @param r1 First Rectangle
+     * @param r2 Second Rectangle
+     * @return  true if they intersect, false if not.
+     */
+    static bool rectangles_intersect(Rectangle &r1, Rectangle &r2) {
         return r1.intersects(r2);
     }
 
+    /**
+     * Function that takes care of all the prompt for inputting rectangle
+     * date.
+     *
+     * Made it to a separate function to get rid of code duplication when asking for data
+     * of multiple rectangles.
+     *
+     * @return Returns an instance of Rectangle that used input data.
+     */
     static Rectangle askForRectInput() {
         double x, y;
         int length, width;
@@ -119,6 +219,17 @@ public:
         return *new Rectangle(Point(x, y), length, width);;
     }
 
+    /**
+     * Function that prompts the user for a number.
+     *
+     * This function will not exit until the user enters a valid number. This function
+     * returns a double to "cover" all of the possible numbers.
+     *
+     * If an integer is needed, just cast it to an int--with the consequence of doing so in mind.
+     *
+     * @param prompt Prompt to be printed out before opening up cin.
+     * @return return the inputted number.
+     */
     static double askForNumInput(char const *prompt) {
         cout << prompt;
         double choice;
@@ -135,6 +246,11 @@ public:
     }
 };
 
+/*
+ * Main method for the class.
+ *
+ * statically call start() from Assignment1 class.
+ */
 int main() {
     Assignment1::start();
 }
